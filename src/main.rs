aa61bf21156dcd3362c75952e8fe033a5b0358e0
markisha64 <marko.grizelj03@gmail.com>
2023-08-06 12:19:21 +0200

    feat: initial cli part

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use chrono::Utc;
+use clap::Parser;
+
+/// Randomize dmw3
+#[derive(Parser)]
+struct Arguments {
+    /// iso path
+    path: std::path::PathBuf,
+    /// randomization seed
+    #[clap(long, default_value_t = Utc::now().timestamp().try_into().unwrap())]
+    seed: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+
+    println!("{pa}", pa = args.seed);
+    println!("{pa}", pa = args.path.to_str().unwrap());
 }
