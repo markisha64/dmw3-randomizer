e75c2d363cfeb1ade5b8a717d5ab4b9d1f80e500
markisha64 <marko.grizelj03@gmail.com>
2023-09-11 11:00:10 +0200

    fix: added invalid digimon index

--- src/rand/parties.rs
+++ src/rand/parties.rs
@@ -6,14 +6,14 @@ use super::Objects;
 
 pub fn patch(preset: &Randomizer, objects: &mut Objects, rng: &mut Xoshiro256StarStar) {
     let parties = &mut objects.parties.modified;
-    let mut all_digimon: [u8; 9] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    let rindex = (rng.next_u64() % 7) as usize;
+    let mut all_digimon: [u8; 8] = [0, 1, 2, 3, 4, 5, 6, 7];
+    let rindex = (rng.next_u64() % 6) as usize;
     for i in 0..3 {
         // Fisher-Yates shuffles
         for _ in 0..preset.shuffles {
-            for j in 0..7 {
+            for j in 0..6 {
                 let uniform = rng.next_u64() as usize;
-                let k = j + uniform % (8 - j);
+                let k = j + uniform % (7 - j);
 
                 all_digimon.swap(j, k);
             }
