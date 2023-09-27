50469fbe978863a2cbe5409110c8325b6bf748be
markisha64 <marko.grizelj03@gmail.com>
2023-09-27 18:23:10 +0200

    feat: gui stat / res distribution

--- src/gui/parties.rs
+++ src/gui/parties.rs
@@ -2,7 +2,7 @@ use dioxus::prelude::*;
 
 use crate::json::Preset;
 
-use crate::gui::checkbox;
+use crate::gui::{checkbox, number_field};
 
 pub fn parties(cx: Scope) -> Element {
     let state = use_shared_state::<Preset>(cx).unwrap();
@@ -10,6 +10,14 @@ pub fn parties(cx: Scope) -> Element {
 
     let enabled = read_state.randomizer.party.enabled;
     let random_parties = read_state.randomizer.party.random_parties;
+
+    let stat_distribution = read_state.randomizer.party.random_stat_distribution;
+    let min_stat = read_state.randomizer.party.min_starting_stat;
+    let total_start_stat = read_state.randomizer.party.total_starting_stats;
+
+    let res_distribution = read_state.randomizer.party.random_res_distribution;
+    let min_res = read_state.randomizer.party.min_starting_res;
+    let total_start_res = read_state.randomizer.party.total_starting_res;
 
     render! {
         div {
@@ -24,8 +32,6 @@ pub fn parties(cx: Scope) -> Element {
                         state.write().randomizer.party.enabled = x.data.value == "true";
                     }
                 },
-            },
-            div {
                 checkbox::checkbox {
                     label: "Parties",
                     id: "parties.random_parties",
@@ -36,7 +42,125 @@ pub fn parties(cx: Scope) -> Element {
                         state.write().randomizer.party.random_parties = x.data.value == "true";
                     }
                 },
-            }
+            },
+            div {
+                class: "left",
+                checkbox::checkbox {
+                    label: "Stat distribution",
+                    id: "parties.stat_distribution",
+                    checked: stat_distribution,
+                    disabled: !enabled,
+                    tooltip: "Randomize player digimon stat distribution",
+                    onchange: move |x: Event<FormData>| {
+                        state.write().randomizer.party.random_stat_distribution = x.data.value == "true";
+                    }
+                },
+                number_field::number_field {
+                    label: "Min stat",
+                    id: "parties.min_stat",
+                    value: min_stat as i64,
+                    disabled: !stat_distribution || !enabled,
+                    min: 1,
+                    max: (total_start_stat / 5) as i64,
+                    tooltip: "Mininum points per stat",
+                    onchange: move |x: Event<FormData>| {
+                        let new_stat = match x.data.value.parse::<u16>() {
+                            Ok(s) => {
+                                if s * 5 > total_start_stat {
+                                    min_stat
+                                } else {
+                                    s
+                                }
+                            },
+                            _ => min_stat
+                        };
+
+                        state.write().randomizer.party.min_starting_stat = new_stat
+                    }
+                },
+                number_field::number_field {
+                    label: "Total stats",
+                    id: "parties.total_stats",
+                    value: total_start_stat as i64,
+                    disabled: !stat_distribution || !enabled,
+                    min: (min_stat * 5) as i64,
+                    max: 4995,
+                    tooltip: "Total starting stats",
+                    onchange: move |x: Event<FormData>| {
+                        let new_stat = match x.data.value.parse::<u16>() {
+                            Ok(s) => {
+                                if s >= min_stat * 5 {
+                                    s
+                                } else {
+                                    total_start_stat
+                                }
+                            },
+                            _ => total_start_stat
+                        };
+
+                        state.write().randomizer.party.total_starting_stats = new_stat
+                    }
+                }
+            },
+            div {
+                class: "left",
+                checkbox::checkbox {
+                    label: "Res distribution",
+                    id: "parties.res_distribution",
+                    checked: res_distribution,
+                    disabled: !enabled,
+                    tooltip: "Randomize player digimon res distribution",
+                    onchange: move |x: Event<FormData>| {
+                        state.write().randomizer.party.random_res_distribution = x.data.value == "true";
+                    }
+                },
+                number_field::number_field {
+                    label: "Min res",
+                    id: "parties.min_res",
+                    value: min_res as i64,
+                    disabled: !res_distribution || !enabled,
+                    min: 1,
+                    max: (total_start_res / 7) as i64,
+                    tooltip: "Mininum points per res",
+                    onchange: move |x: Event<FormData>| {
+                        let new_res = match x.data.value.parse::<u16>() {
+                            Ok(s) => {
+                                if s * 7 > total_start_res {
+                                    min_res
+                                } else {
+                                    s
+                                }
+                            },
+                            _ => min_res
+                        };
+
+                        state.write().randomizer.party.min_starting_res = new_res
+                    }
+                },
+                number_field::number_field {
+                    label: "Total res",
+                    id: "parties.total_res",
+                    value: total_start_res as i64,
+                    disabled: !res_distribution || !enabled,
+                    min: (min_res * 7) as i64,
+                    max: 6993,
+                    tooltip: "Total starting res",
+                    onchange: move |x: Event<FormData>| {
+                        let new_res = match x.data.value.parse::<u16>() {
+                            Ok(s) => {
+                                if s >= min_res * 7 {
+                                    s
+                                } else {
+                                    total_start_res
+                                }
+                            },
+                            _ => total_start_res
+                        };
+
+                        state.write().randomizer.party.total_starting_res = new_res
+                    }
+                }
+            },
         }
     }
 }
